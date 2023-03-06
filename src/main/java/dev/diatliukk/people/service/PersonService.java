55757c63bdbc8ab58f88f0fcdbef3ad00050a97d
Ivan Diatliuk <dyatlyukk.ivan@gmail.com>
2023-03-06 17:18:03 +0200

    Add person service

---
+++ src/main/java/dev/diatliukk/people/service/PersonService.java
@@ -0,0 +1,31 @@
+package dev.diatliukk.people.service;
+
+import dev.diatliukk.people.entity.PersonEntity;
+import dev.diatliukk.people.model.Person;
+import dev.diatliukk.people.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PersonService {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    public Iterable<PersonEntity> getPeople() {
+        return personRepository.findAll();
+    }
+
+    public Optional<Person> getPersonById(Integer id) {
+        return personRepository.findById(id).isPresent() ?
+                Optional.of(Person.toModel(personRepository.findById(id).get())) :
+                Optional.empty();
+    }
+
+    public void saveOrUpdate(PersonEntity person) {
+        personRepository.save(person);
+    }
+
+}
