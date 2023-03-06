75fb6f94439c76a58de61a162700c3020d6c9360
Ivan Diatliuk <dyatlyukk.ivan@gmail.com>
2023-03-06 10:32:34 +0200

    The initial project

---
+++ src/main/java/dev/diatliukk/people/PeopleBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.diatliukk.people;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeopleBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/diatliukk/people/PeopleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.diatliukk.people;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
