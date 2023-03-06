f33ebb556209ffdf2afa4769b0fc3d2fdb2638f4
Ivan Diatliuk <dyatlyukk.ivan@gmail.com>
2023-03-06 17:14:46 +0200

    Add Person model to represent data

---
+++ src/main/java/dev/diatliukk/people/model/Person.java
@@ -0,0 +1,30 @@
+package dev.diatliukk.people.model;
+
+import dev.diatliukk.people.entity.PersonEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private Integer age;
+
+    public static Person toModel(PersonEntity personEntity) {
+        return new Person(personEntity.getId(), personEntity.getFirstName(), personEntity.getLastName(), calculateAge(personEntity.getDateOfBirth()) );
+    }
+
+    private static Integer calculateAge(LocalDate dateOfBirth) {
+        LocalDate currentDate = LocalDate.now();
+        return Period.between(dateOfBirth, currentDate).getYears();
+    }
+}
